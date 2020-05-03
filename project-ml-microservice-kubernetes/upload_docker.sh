ad9aecc8cbf71c4b1f3440516cc0923cdc0d615c
Ubuntu <ubuntu@ip-172-31-20-144.us-west-2.compute.internal>
2020-05-03 07:01:38 +0000

    feat: worked on steps in the given assignment

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="ezeebube5/ml-microservice:latest"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login &&\
+  docker image tag ml-microservice $dockerpath
+  
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
